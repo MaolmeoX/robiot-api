57f9e78721148ecfad1e505abc5c8a60a19f77ad
David Lagalle <32126427+David-Lagalle@users.noreply.github.com>
2020-07-29 13:54:42 +0200

    get and put works with robiot API

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,39 +1,48 @@
-const axios = require('axios').default;
+//const axios = require('axios').default;
+import axios, { AxiosResponse } from 'axios';
 
-const uriAPI = 'http://localhost:3000/api-docs/';
+const uriAPI = 'http://localhost:8080/ecotree/robiot-api'; //One API per Robiot !
 
-export function getAction(link: string) {
+export function getRobiotPosition() {
     return axios
-        .get(`${uriAPI}/${link}`, {
-            params: {
-                //ID: 12345,
-            },
+        .get(`${uriAPI}/configurations/301`, {
+            params: {},
         })
-        .then(function (response: string) {
-            console.log(response);
+        .then(function (response: AxiosResponse) {
+            console.log(response.data);
         })
         .catch(function (error: string) {
             console.log(error);
-        })
-        .finally(function () {
-            // always executed
         });
 }
 
-export function putAction(link: string) {
+export async function getRobiotBattery() {
+    return await axios
+        .get(`${uriAPI}/configurations/101`, {
+            params: {},
+        })
+        .then(function (response: AxiosResponse) {
+            return response.data;
+        })
+        .catch(function (error: string) {
+            console.log(error);
+        });
+}
+
+export function moveRobiot(x: string, y: string) {
     return axios
-        .put(`${uriAPI}/${link}`, {
-            id: 302,
+        .put(`${uriAPI}/configurations/302`, {
+            id: '302',
             link: {
                 params: { rel: 'self' },
-                href: link,
+                href: '/configurations/302',
             },
             name: 'Destination Location',
-            content: '2.0,2.0',
+            content: `${x},${y}`, //ex: 2.0,2.0
             status: 'ACTIVE',
         })
-        .then(function (response: string) {
-            console.log(response);
+        .then(function (response: AxiosResponse) {
+            console.log(response.data);
         })
         .catch(function (error: string) {
             console.log(error);
